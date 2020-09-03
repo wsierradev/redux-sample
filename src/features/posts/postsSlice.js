e530ef68295c883dcd132752498a4db3cd8cfed6
William Sierra <tealsasuke@aol.com>
2020-09-03 19:18:57 -0400

    test commit

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
+
+
+
+const initialState = [
+  { id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), { minutes: 10 }).toISOString()},
+  { id: '2', title: 'Second Post', content: 'More text',  date: sub(new Date(), { minutes: 10 }).toISOString()}
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          },
+        }
+      },
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find(post => post.id === id)
+        if (existingPost) {
+          existingPost.title = title
+          existingPost.content = content
+        }
+    }
+  }
+})
+
+export const { postAdded, postUpdated } = postsSlice.actions
+
+
+export default postsSlice.reducer
